3b097d01b59a11358a2939e6ef6bfa558ceeb72a
Lyle <lyle@mytijian.com>
2018-02-26 22:08:57 +0800

    use bulma as css framework

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Hi, This is Kong speaking.
-        </p> 
+         <section class="section">
+          <div class="container">
+            <h1 class="title">
+              Hello World
+            </h1>
+            <p class="subtitle">
+              My first website with <strong>Bulma</strong>!
+            </p>
+          </div>
+        </section>
       </div>
     );
   }
